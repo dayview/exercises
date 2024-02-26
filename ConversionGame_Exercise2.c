f0233eb08c6fb0238dc41e7c41c49c1c05951cf2
dayview <leon_pavino@dlsu.edu.ph>
2024-02-26 21:13:47 +0800

    Updated correct info

--- ConversionGame_Exercise2.c
+++ ConversionGame_Exercise2.c
@@ -14,38 +14,34 @@ int main() {
     int bronzePerSilver = 50;
     int copperPerBronze = 150;
 
-    int silverPrice1 = goldPrice1 * silverPerGold;
-    int bronzePrice1 = silverPrice1 * bronzePerSilver;
-    int copperPrice1 = bronzePrice1 * copperPerBronze;
+    float silverPrice1 = goldPrice1 * silverPerGold;
+    float bronzePrice1 = silverPrice1 * bronzePerSilver;
+    float copperPrice1 = bronzePrice1 * copperPerBronze;
 
-    int silverPrice2 = goldPrice2 * silverPerGold;
-    int bronzePrice2 = silverPrice2 * bronzePerSilver;
-    int copperPrice2 = bronzePrice2 * copperPerBronze;
+    float silverPrice2 = goldPrice2 * silverPerGold;
+    float bronzePrice2 = silverPrice2 * bronzePerSilver;
+    float copperPrice2 = bronzePrice2 * copperPerBronze;
 
-    int goldPrice1 = goldPrice1 * (100 - discount) / 100;
-    int goldPrice2 = goldPrice2 * (100 - discount) / 100;
+    float goldDiscountedPrice1 = goldPrice1 * (100 - discount) / 100;
+    float goldDiscountedPrice2 = goldPrice2 * (100 - discount) / 100;
 
-    int silverPrice1 = silverPrice1 * (100 - discount) / 100;
-    int silverPrice2 = silverPrice2 * (100 - discount) / 100;
+    float silverDiscountedPrice1 = silverPrice1 * (100 - discount) / 100;
+    float silverDiscountedPrice2 = silverPrice2 * (100 - discount) / 100;
 
-    int bronzePrice1 = bronzePrice1 * (100 - discount) / 100;
-    int bronzePrice2 = bronzePrice2 * (100 - discount) / 100;
+    float bronzeDiscountedPrice1 = bronzePrice1 * (100 - discount) / 100;
+    float bronzeDiscountedPrice2 = bronzePrice2 * (100 - discount) / 100;
 
-    int copperPrice1 = copperPrice1 * (100 - discount) / 100;
-    int copperPrice2 = copperPrice2 * (100 - discount) / 100;
+    float copperDiscountedPrice1 = copperPrice1 * (100 - discount) / 100;
+    float copperDiscountedPrice2 = copperPrice2 * (100 - discount) / 100;
 
     printf("\nOriginal Prices:\n");
-    printf("Item 1: %d gold, %d silver, %d bronze, %d copper\n",
-           (goldPrice1, silverPrice1, bronzePrice1, copperPrice1);
-    printf("Item 2: %d gold, %d silver, %d bronze, %d copper\n",
-           (goldPrice2, silverPrice2, bronzePrice2, copperPrice2);
+    printf("Item 1: %.2f gold, %.2f silver, %.2f bronze, %.2f copper\n", goldPrice1, silverPrice1, bronzePrice1, copperPrice1);
+    printf("Item 2: %.2f gold, %.2f silver, %.2f bronze, %.2f copper\n", goldPrice2, silverPrice2, bronzePrice2, copperPrice2);
 
     // Print discounted prices
     printf("\nDiscounted Prices:\n");
-    printf("Item 1: %d gold, %d silver, %d bronze, %d copper\n",
-           (goldPrice1, silverPrice1, bronzePrice1, copperPrice1);
-    printf("Item 2: %d gold, %d silver, %d bronze, %d copper\n",
-           (goldPrice2, silverPrice2, bronzePrice2, copperPrice2);
+    printf("Item 1: %.2f gold, %.2f silver, %.2f bronze, %.2f copper\n", goldDiscountedPrice1, silverDiscountedPrice1, bronzeDiscountedPrice1, copperDiscountedPrice1);
+    printf("Item 2: %.2f gold, %.2f silver, %.2f bronze, %.2f copper\n", goldDiscountedPrice2, silverDiscountedPrice2, bronzeDiscountedPrice2, copperDiscountedPrice2);
 
     return 0;
 }
