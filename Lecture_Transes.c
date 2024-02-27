733f8cfc86e6428b2eef3126fb1abba5a11120f8
dayview <leon_pavino@dlsu.edu.ph>
2024-02-28 01:46:38 +0800

    Transes update

---
+++ Lecture_Transes.c
@@ -0,0 +1,10 @@
+int main(){
+    /* variable declaration */
+    float fBase, fHeight, fArea;
+
+    /* statements */
+    fBase = 10.5;
+    fHeight = 5.0;
+
+    fArea = fBase * fHeight;
+    return 0;
