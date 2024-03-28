68a1957cfa43972114ae95cd9eeada391fbaa6b6
dayview <leon_pavino@dlsu.edu.ph>
2024-03-28 21:18:20 +0800

    MP Update

--- MP_3YT2/MP_226v2.c
+++ MP_3YT2/MP_226v2.c
@@ -303,10 +303,10 @@ int main(){
 
     while (!winner){     
         displayBoard(&a1, &a2, &a3, &a4, &a5, 
-                    &b1, &b2, &b3, &b4, &b5, 
-                    &c1, &c2, &c3, &c4, &c5, 
-                    &d1, &d2, &d3, &d4, &d5, 
-                    &e1, &e2, &e3, &e4, &e5);
+                     &b1, &b2, &b3, &b4, &b5, 
+                     &c1, &c2, &c3, &c4, &c5, 
+                     &d1, &d2, &d3, &d4, &d5, 
+                     &e1, &e2, &e3, &e4, &e5);
         int move = getMove(&player, &nPlayersChoice, &nSpecialChoice, 
                            &a1, &a2, &a3, &a4, &a5, 
                            &b1, &b2, &b3, &b4, &b5, 
