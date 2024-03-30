907110d5afcdbafd1c57084d20b643c580ba4d5f
dayview <leon_pavino@dlsu.edu.ph>
2024-03-30 23:03:34 +0800

    MP critical (2) update

--- MP_3YT2/MP_226v5.c
+++ MP_3YT2/MP_226v5.c
@@ -3,213 +3,6 @@
 
 #define BOARD_SIZE 5
 
-void displayStart(int *numPlayers, int *useSpecialRules){
-    printf("Welcome to Tic-Tac-Toe!\n\n");
-    printf("How many players will be playing?\n");
-    printf("----------------------\n");
-    printf("| [1] Two Players    |\n");
-    printf("| [2] Three Players  |\n");
-    printf("----------------------\n\n");
-    printf("Your choice: ");
-    scanf("%d", numPlayers);
-
-    printf("Would you like to play with special rules?\n");
-    printf("----------------------\n");
-    printf("| [1] Yes            |\n");
-    printf("| [2] No             |\n");
-    printf("----------------------\n\n");
-    printf("Your choice: ");
-    scanf("%d", useSpecialRules);
-}
-
-void setupGame(int numPlayers, int useSpecialRules){
-    printf("\nSetting up the game...\n");
-
-    if (numPlayers == 1) {
-        printf("Two players will be playing.\n");
-    } else {
-        printf("Three players will be playing.\n");
-    }
-
-    if (useSpecialRules == 1) {
-        printf("Special rules will be used.\n");
-    } else {
-        printf("No special rules will be applied.\n");
-    }
-
-    printf("Game setup complete.\n\n");
-}
-
-char getSymbol(int cell){
-    if (cell == 0) {
-        return ' ';
-    } else if (cell == 1) {
-        return 'X';
-    } else if (cell == 2) {
-        return 'O';
-    } else {
-        return 'Y';
-    }
-}
-
-void displayBoard(int *a1, int *a2, int *a3, int *a4, int *a5,
-                int *b1, int *b2, int *b3, int *b4, int *b5,
-                int *c1, int *c2, int *c3, int *c4, int *c5,
-                int *d1, int *d2, int *d3, int *d4, int *d5,
-                int *e1, int *e2, int *e3, int *e4, int *e5){
-
-    printf("\n A | B | C | D | E\n");
-    printf("1  %c| %c| %c| %c| %c\n", getSymbol(*a1), getSymbol(*a2), getSymbol(*a3), getSymbol(*a4), getSymbol(*a5));
-    printf("   6 | 7 | 8 | 9 | 10\n");
-    printf("2  %c| %c| %c| %c| %c\n", getSymbol(*b1), getSymbol(*b2), getSymbol(*b3), getSymbol(*b4), getSymbol(*b5));
-    printf("   11| 12| 13| 14| 15\n");
-    printf("3  %c| %c| %c| %c| %c\n", getSymbol(*c1), getSymbol(*c2), getSymbol(*c3), getSymbol(*c4), getSymbol(*c5));
-    printf("   16| 17| 18| 19| 20\n");
-    printf("4  %c| %c| %c| %c| %c\n", getSymbol(*d1), getSymbol(*d2), getSymbol(*d3), getSymbol(*d4), getSymbol(*d5));
-    printf("   21| 22| 23| 24| 25\n");
-    printf("5  %c| %c| %c| %c| %c\n", getSymbol(*e1), getSymbol(*e2), getSymbol(*e3), getSymbol(*e4), getSymbol(*e5));
-}
-
-int getPlayerMove(int *a1, int *a2, int *a3, int *a4, int *a5,
-            int *b1, int *b2, int *b3, int *b4, int *b5,
-            int *c1, int *c2, int *c3, int *c4, int *c5,
-            int *d1, int *d2, int *d3, int *d4, int *d5,
-            int *e1, int *e2, int *e3, int *e4, int *e5,
-            int player){
-                
-    char row, col;
-    int rowNum, colNum;
-
-    while (1) {
-        printf("Player %d, enter your move (e.g., A1, B3) or 0 for special ability: ", player);
-        scanf(" %c%c", &row, &col);
-
-        if (row >= 'a' && row <= 'e') {
-            rowNum = row - 'a' + 1;
-        } else if (row >= 'A' && row <= 'E') {
-            rowNum = row - 'A' + 1;
-        } else {
-            printf("Invalid row. Please try again.\n");
-        }
-
-        if (rowNum > 0 && col >= '1' && col <= '5'){
-            colNum = col - '0';
-        } else {
-            printf("Invalid move. Please try again.\n");
-        }
-
-        if (rowNum < 1 || rowNum > BOARD_SIZE || colNum < 1 || colNum > BOARD_SIZE){
-            printf("Invalid move. Please try again.\n");
-        } else {
-            int *space = NULL;
-            switch (rowNum){
-                case 1:
-                    switch (colNum){
-                        case 1:
-                            space = a1;
-                            break;
-                        case 2:
-                            space = a2;
-                            break;
-                        case 3:
-                            space = a3;
-                            break;
-                        case 4:
-                            space = a4;
-                            break;
-                        case 5:
-                            space = a5;
-                            break;
-                    }
-                    break;
-                case 2:
-                    switch (colNum){
-                        case 1:
-                            space = b1;
-                            break;
-                        case 2:
-                            space = b2;
-                            break;
-                        case 3:
-                            space = b3;
-                            break;
-                        case 4:
-                            space = b4;
-                            break;
-                        case 5:
-                            space = b5;
-                            break;
-                    }
-                    break;
-                case 3:
-                    switch (colNum){
-                        case 1:
-                            space = c1;
-                            break;
-                        case 2:
-                            space = c2;
-                            break;
-                        case 3:
-                            space = c3;
-                            break;
-                        case 4:
-                            space = c4;
-                            break;
-                        case 5:
-                            space = c5;
-                            break;
-                    }
-                    break;
-                case 4:
-                    switch (colNum){
-                        case 1:
-                            space = d1;
-                            break;
-                        case 2:
-                            space = d2;
-                            break;
-                        case 3:
-                            space = d3;
-                            break;
-                        case 4:
-                            space = d4;
-                            break;
-                        case 5:
-                            space = d5;
-                            break;
-                    }
-                    break;
-                case 5:
-                    switch (colNum){
-                        case 1:
-                            space = e1;
-                            break;
-                        case 2:
-                            space = e2;
-                            break;
-                        case 3:
-                            space = e3;
-                            break;
-                        case 4:
-                            space = e4;
-                            break;
-                        case 5:
-                            space = e5;
-                            break;
-                    }
-                    break;
-            }
-
-            if (*space != 0){
-                printf("That space is already taken. Please try again.\n");
-            } else {
-                *space = player;
-                return (rowNum - 1) * BOARD_SIZE + colNum;
-            }
-        }
-    }
-}
-
 int *findSpacePointer(int *a1, int *a2, int *a3, int *a4, int *a5,
                       int *b1, int *b2, int *b3, int *b4, int *b5,
                       int *c1, int *c2, int *c3, int *c4, int *c5,
@@ -317,6 +110,131 @@ int *findSpacePointer(int *a1, int *a2, int *a3, int *a4, int *a5,
     return space;
 }
 
+void displayStart(int *numPlayers, int *useSpecialRules){
+    printf("Welcome to Tic-Tac-Toe!\n\n");
+    printf("How many players will be playing?\n");
+    printf("----------------------\n");
+    printf("| [1] Two Players    |\n");
+    printf("| [2] Three Players  |\n");
+    printf("----------------------\n\n");
+    printf("Your choice: ");
+    scanf("%d", numPlayers);
+
+    printf("Would you like to play with special rules?\n");
+    printf("----------------------\n");
+    printf("| [1] Yes            |\n");
+    printf("| [2] No             |\n");
+    printf("----------------------\n\n");
+    printf("Your choice: ");
+    scanf("%d", useSpecialRules);
+}
+
+void setupGame(int numPlayers, int useSpecialRules){
+    printf("\nSetting up the game...\n");
+
+    if (numPlayers == 1) {
+        printf("Two players will be playing.\n");
+    } else {
+        printf("Three players will be playing.\n");
+    }
+
+    if (useSpecialRules == 1) {
+        printf("Special rules will be used.\n");
+    } else {
+        printf("No special rules will be applied.\n");
+    }
+
+    printf("Game setup complete.\n\n");
+}
+
+char getSymbol(int cell){
+    if (cell == 0) {
+        return ' ';
+    } else if (cell == 1) {
+        return 'X';
+    } else if (cell == 2) {
+        return 'O';
+    } else {
+        return 'Y';
+    }
+}
+
+void displayBoard(int *a1, int *a2, int *a3, int *a4, int *a5,
+                int *b1, int *b2, int *b3, int *b4, int *b5,
+                int *c1, int *c2, int *c3, int *c4, int *c5,
+                int *d1, int *d2, int *d3, int *d4, int *d5,
+                int *e1, int *e2, int *e3, int *e4, int *e5){
+
+    printf("\n     A   B   C   D   E\n");
+    printf("1 | %c | %c | %c | %c | %c |\n", getSymbol(*a1), getSymbol(*a2), getSymbol(*a3), getSymbol(*a4), getSymbol(*a5));
+    printf("   --------------------\n");
+    printf("2 | %c | %c | %c | %c | %c |\n", getSymbol(*b1), getSymbol(*b2), getSymbol(*b3), getSymbol(*b4), getSymbol(*b5));
+    printf("   --------------------\n");
+    printf("3 | %c | %c | %c | %c | %c |\n", getSymbol(*c1), getSymbol(*c2), getSymbol(*c3), getSymbol(*c4), getSymbol(*c5));
+    printf("   --------------------\n");
+    printf("4 | %c | %c | %c | %c | %c |\n", getSymbol(*d1), getSymbol(*d2), getSymbol(*d3), getSymbol(*d4), getSymbol(*d5));
+    printf("   --------------------\n");
+    printf("5 | %c | %c | %c | %c | %c |\n", getSymbol(*e1), getSymbol(*e2), getSymbol(*e3), getSymbol(*e4), getSymbol(*e5));
+    printf("   --------------------\n");
+}
+
+int *getPlayerMove(int *a1, int *a2, int *a3, int *a4, int *a5,
+            int *b1, int *b2, int *b3, int *b4, int *b5,
+            int *c1, int *c2, int *c3, int *c4, int *c5,
+            int *d1, int *d2, int *d3, int *d4, int *d5,
+            int *e1, int *e2, int *e3, int *e4, int *e5,
+            int player){
+                
+    char rowChar, colChar;
+    int rowNum, colNum;
+    int validMove = 0;
+    int validInput = 1;
+
+    while (!validMove) {
+        printf("Player %d, enter your move (e.g., A1, B3) or 0 for special ability: ", player);
+        scanf(" %c%c", &rowChar, &colChar);
+
+        if (rowChar >= 'a' && rowChar <= 'e') {
+            rowNum = rowChar - 'a' + 1;
+        } else if (rowChar >= 'A' && rowChar <= 'E') {
+            rowNum = rowChar - 'A' + 1;
+        } else {
+            printf("Invalid row. Please try again.\n");
+            validInput = 0;
+        }
+
+        if (validInput && rowNum > 0 && colChar >= '1' && colChar <= '5'){
+            colNum = colChar - '0';
+        } else {
+            printf("Invalid move. Please try again.\n");
+            validInput = 0;
+        }
+
+        if (validInput && (rowNum < 1 || rowNum > BOARD_SIZE || colNum < 1 || colNum > BOARD_SIZE)){
+            printf("Invalid move. Please try again.\n");
+            validInput = 0;
+        }
+
+        if (validInput){
+            int *space = findSpacePointer(a1, a2, a3, a4, a5,
+                                          b1, b2, b3, b4, b5,
+                                          c1, c2, c3, c4, c5,
+                                          d1, d2, d3, d4, d5,
+                                          e1, e2, e3, e4, e5,
+                                          rowNum, colNum);
+
+            if (*space != 0){
+                printf("That space is already taken. Please try again.\n");
+            } else {
+                validMove = 1;
+                return space;
+            }
+        }
+        validInput = 1;
+    }
+    return NULL;
+}
+
 void handleSpecialAbility(int *a1, int *a2, int *a3, int *a4, int *a5,
                           int *b1, int *b2, int *b3, int *b4, int *b5,
                           int *c1, int *c2, int *c3, int *c4, int *c5,
@@ -350,6 +268,12 @@ void handleSpecialAbility(int *a1, int *a2, int *a3, int *a4, int *a5,
             if (*space != 0 && *space != player) {
                 *space = 0;
                 printf("Space erased successfully.\n");
+
+                displayBoard(a1, a2, a3, a4, a5,
+                             b1, b2, b3, b4, b5,
+                             c1, c2, c3, c4, c5,
+                             d1, d2, d3, d4, d5,
+                             e1, e2, e3, e4, e5);
             } else {
                 printf("Invalid space or your own space. Try again.\n");
             }
@@ -409,6 +333,11 @@ void handleSpecialAbility(int *a1, int *a2, int *a3, int *a4, int *a5,
                         break;
                 }
                 printf("Row %d erased successfully.\n", row);
+                displayBoard(a1, a2, a3, a4, a5,
+                             b1, b2, b3, b4, b5,
+                             c1, c2, c3, c4, c5,
+                             d1, d2, d3, d4, d5,
+                             e1, e2, e3, e4, e5);
             } else {
                 printf("Invalid row. Please try again.\n");
             }
@@ -455,6 +384,11 @@ void handleSpecialAbility(int *a1, int *a2, int *a3, int *a4, int *a5,
                         break;
                 }
                 printf("Column %d erased successfully.\n", col);
+                displayBoard(a1, a2, a3, a4, a5,
+                             b1, b2, b3, b4, b5,
+                             c1, c2, c3, c4, c5,
+                             d1, d2, d3, d4, d5,
+                             e1, e2, e3, e4, e5);
             } else {
                 printf("Invalid column. Please try again.\n");
             }
@@ -560,13 +494,14 @@ int main(){
                      &c1, &c2, &c3, &c4, &c5, 
                      &d1, &d2, &d3, &d4, &d5, 
                      &e1, &e2, &e3, &e4, &e5);
-        int move = getPlayerMove(&a1, &a2, &a3, &a4, &a5, 
+                     eraseSpaceUsed = 0;
+        int *move = getPlayerMove(&a1, &a2, &a3, &a4, &a5, 
                                  &b1, &b2, &b3, &b4, &b5, 
                                  &c1, &c2, &c3, &c4, &c5, 
                                  &d1, &d2, &d3, &d4, &d5, 
                                  &e1, &e2, &e3, &e4, &e5, player);
-        updateBoard(&move, player);
-        player = (player % nPlayersChoice - 1) + 1;
+        updateBoard(move, player);
+        player = (player % nPlayersChoice) + 1;
 
         winner = checkWin(a1, a2, a3, a4, a5, 
                           b1, b2, b3, b4, b5, 
@@ -580,6 +515,7 @@ int main(){
                                 printf("Player %d wins!\n", winner);
                                 gameEnded = 1;
                             }
+
     }
     return 0;
 }
