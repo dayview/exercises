373ff4efe65857650b158aedba8d404f3a2c06ca
dayview <leon_pavino@dlsu.edu.ph>
2024-03-30 19:52:51 +0800

    MP Update

--- MP_3YT2/MP_226v4.c
+++ MP_3YT2/MP_226v4.c
@@ -450,13 +450,12 @@ int main(){
                      &d1, &d2, &d3, &d4, &d5, 
                      &e1, &e2, &e3, &e4, &e5);
         int move = getPlayerMove(&a1, &a2, &a3, &a4, &a5, 
-                           &b1, &b2, &b3, &b4, &b5, 
-                           &c1, &c2, &c3, &c4, &c5, 
-                           &d1, &d2, &d3, &d4, &d5, 
-                           &e1, &e2, &e3, &e4, &e5,
-                           player);
+                                 &b1, &b2, &b3, &b4, &b5, 
+                                 &c1, &c2, &c3, &c4, &c5, 
+                                 &d1, &d2, &d3, &d4, &d5, 
+                                 &e1, &e2, &e3, &e4, &e5, player);
         updateBoard(&move, player);
-        player = (player % nPlayersChoice) + 1;
+        player = (player % (numPlayers - 1)) + 1;
 
         winner = checkWin(a1, a2, a3, a4, a5, 
                           b1, b2, b3, b4, b5, 
